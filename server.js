dafae41b349148f845e4bfbbb6c7761b2e42993f
Sarah Rehan <sarahahmad17@gmail.com>
2023-10-24 23:18:20 -0400

    changed url to URL

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/', async (req, res) => {
       res.status(200).render('index', { urls, message })
     }
     else {
-      res.status(404).json("No urls found")
+      res.status(404).json("No URL found")
     }
     
   }
@@ -54,15 +54,14 @@ app.get('/', async (req, res) => {
 app.post('/', (req, res) => {
   const original_url = req.body.urlInput
   if (!isValidUrl(original_url)) {
-    req.flash('msg', "Invalid Url! Please enter a url with valid protocol.");
+    req.flash('msg', "Invalid URL! Please enter a URL with valid protocol.");
     res.redirect('/')
   }
   else {
     try {
       UrlModel.findOne({ original_url }).then((result) => {
         if (result !== null) {
-          // res.json(result)
-          req.flash('msg', "Url already exists!");
+          req.flash('msg', "URL already exists!");
           res.redirect('/')
         }
         else {
@@ -72,7 +71,7 @@ app.post('/', (req, res) => {
             short_url,
           })
           newUrl.save()
-          req.flash('msg', "Url added successfully!");
+          req.flash('msg', "URL added successfully!");
           res.redirect('/')
         }
       })
@@ -91,7 +90,7 @@ app.get('/:short_url', (req, res) => {
       res.redirect(data.original_url)
     })
     .catch((err) =>
-      res.status(404).json("Url not found")
+      res.status(404).json("URL not found")
     )
   }
   catch (err) {
@@ -100,13 +99,12 @@ app.get('/:short_url', (req, res) => {
 })
 
 app.post('/delete/:id', async (req, res) => {
-// app.delete('/:id', async (req, res) => {
   try {
     const url = await UrlModel.deleteOne({_id: req.params.id})
     if (!url){
-      return res.status(400).json('Url not found')
+      return res.status(400).json('URL not found')
     }
-    req.flash('msg', "Url deleted successfully!");
+    req.flash('msg', "URL deleted successfully!");
     res.redirect('/')
   }
   catch (err) {
